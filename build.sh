eff281dcffaf584183b685249f449681000d3a78
Jonathan Chiu <jchiu09@gmail.com>
2023-01-22 16:30:49 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -89,7 +89,7 @@ Build()
 YarnInstall()
 {
     ProgressStart 'yarn install'
-    yarn install --frozen-lockfile --network-timeout 120000
+    yarn install --ignore-engines --frozen-lockfile --network-timeout 120000
     ProgressEnd 'yarn install'
 }
 
